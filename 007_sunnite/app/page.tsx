8c94abd468981b596df7eaca59a71d5dbea232c0
Lane Henslee <huncho@hygo.ai>
2024-08-24 00:58:31 -0500

    added the buttons

--- 007_sunnite/app/page.tsx
+++ 007_sunnite/app/page.tsx
@@ -1,10 +1,20 @@
 import Image from "next/image";
+import { FaCopy } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <div className="w-full justify-center">
-      <button className="big-button">Telegram</button>
-      <button className="big-button">Buy Now</button>
+    <div>
+      <div className="mb-8 w-full justify-center flex gap-3">
+        <button className="big-button">Telegram</button>
+        <button className="big-button">Buy Now</button>
+      </div>
+      <div className="w-96 mx-auto">
+        <div className="w-full text-center">Contract Address</div>
+        <div className="h-12 bg-primary rounded border border-border flex">
+          <div className="my-auto w-full">asdfasdfasdfasdf</div>
+          <FaCopy className="-ml-8 my-auto text-secondary hover:text-secondary-dark cursor-pointer" />
+        </div>
+      </div>
     </div>
   );
 }

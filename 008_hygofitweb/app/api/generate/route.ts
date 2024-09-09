094749d4dbcce7a2aa0ced4919f53486490594d0
Lane Henslee <huncho@hygo.ai>
2024-09-09 07:42:58 -0700

    got the schema right

--- 008_hygofitweb/app/api/generate/route.ts
+++ 008_hygofitweb/app/api/generate/route.ts
@@ -2,21 +2,22 @@ import OpenAI from "openai";
 import { zodResponseFormat } from "openai/helpers/zod.mjs";
 import { z } from "zod";
 
-const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-const SCHEMA = z.array(
-  z.object({
-    name: z.string(),
-    sets: z.array(
-      z.object({
-        reps: z.number().int(),
-      })
-    ),
-  })
-);
+const SCHEMA = z.object({
+  data: z.array(
+    z.object({
+      name: z.string(),
+      sets: z.array(
+        z.object({
+          reps: z.number().int(),
+        })
+      ),
+    })
+  ),
+});
 
 export async function POST(request: Request) {
   const contentType = request.headers.get("Content-Type");
@@ -38,8 +39,7 @@ export async function POST(request: Request) {
   }
 
   const muscleGroupsString = muscleGroups.join(", ");
-  console.log(OPENAI_API_KEY);
-  const message = `Please create a workout in the style of ${athlete} with the following muscle groups: ${muscleGroupsString} that will take duration of ${duration} minutes to complete. Return a list of exercises in json format.`;
+  const message = `Please create a workout in the style of ${athlete} with the following muscle groups: ${muscleGroupsString} that will take duration of ${duration} minutes to complete. Return a list of exercises in json format. Each exersie has a name and list of sets with reps.`;
   const openaiResponse = await openai.chat.completions.create({
     model: "gpt-4o-mini",
     response_format: zodResponseFormat(SCHEMA, "exercises"),
@@ -60,8 +60,7 @@ export async function POST(request: Request) {
     ],
   });
   const content = openaiResponse.choices[0].message.content;
-  console.log(content);
-  return new Response(JSON.stringify(content), {
+  return new Response(content, {
     headers: {
       "content-type": "application/json",
     },
